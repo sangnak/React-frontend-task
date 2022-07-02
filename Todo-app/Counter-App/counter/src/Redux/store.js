6ee9302d99fbd6dc048f2df17a21765b7981972d
AbhaySatpute <100846201+AbhaySatpute@users.noreply.github.com>
2022-07-02 09:13:23 +0530

    Delete Todo-app/Counter-App/counter directory

--- Todo-app/Counter-App/counter/src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-
-import {createStore } from 'redux'
-import { Reducer } from './reducer'
-
-export const store = createStore(Reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-
-console.log("entire Store is here Veeru: " , store.getState())
